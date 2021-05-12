d58731237c86e77495d62d3e8d991fd5624d5edb
Julia Groza <jgroza@uwo.ca>
2021-05-12 11:58:21 -0400

    Adding my files

---
+++ start/contract.js
@@ -0,0 +1,93 @@
+/*global ethereum, MetamaskOnboarding */
+
+/*
+The `piggybankContract` is compiled from:
+
+  pragma solidity ^0.4.0;
+  contract PiggyBank {
+
+      uint private balance;
+      address public owner;
+
+      function PiggyBank() public {
+          owner = msg.sender;
+          balance = 0;
+      }
+
+      function deposit() public payable returns (uint) {
+          balance += msg.value;
+          return balance;
+      }
+
+      function withdraw(uint withdrawAmount) public returns (uint remainingBal) {
+          require(msg.sender == owner);
+          balance -= withdrawAmount;
+
+          msg.sender.transfer(withdrawAmount);
+
+          return balance;
+      }
+  }
+*/
+
+const forwarderOrigin = 'http://localhost:9010'
+
+const initialize = () => {
+  // Basic Actions Section
+  const onboardButton = document.getElementById('connectButton');
+  const getAccountsButton = document.getElementById('getAccounts');
+  const getAccountsResult = document.getElementById('getAccountsResult');
+  
+  // Created check function to see if the MetaMask extension is installed
+  const isMetaMaskInstalled = () => {
+    // Have to check the ethereum binding on the window object to see if it's installed
+    const {ethereum } = window;
+    return Boolean(ethereum && ethereum.isMetaMask);
+
+  };
+
+  // Create a new MetaMask onboarding object to use in our app
+  const onboarding = new MetaMaskOnboarding({ forwarderOrigin });
+
+  // This will start onboarding process
+  const onClickInstall = () => {
+    onboardButton.innerText = 'Onboarding in progress';
+    onboardButton.disabled = true;
+    // On this object, we have startOnboarding which will start the onboarding process
+    onboarding.startOnboarding();
+  };
+
+  const onClickConnect = async () => {
+    try{
+      // Will open the MetaMask UI
+      // You should disable this button while request pending
+      await ethereum.request({method: 'eth_requestAccounts'});
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const MetaMaskClientCheck = () => {
+    if (!isMetaMaskInstalled()){
+      // if it isn't installed, ask user to click to install it
+      onboardButton.innerText = 'Click here to install MetaMask!';
+      onboardButton.onclick = onClickInstall;
+      onboardButton.disabled = false;
+    }
+    else{
+      // if it is installed, we change our button text
+      onboardButton.innerText = 'Connect';
+      onboardButton.onclick = onClickConnect;
+      onboardButton.disabled = false;
+    }
+  };
+  MetaMaskClientCheck();
+  // Eth_Accounts-getAccountsButton
+  getAccountsButton.addEventListener('click', async () => {
+    // we use eth_accounts because it returns a list of addresses owned by us
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
+    // we take the first address in the array of addresses and display it
+    getAccountsResult.innerHTML = accounts[0] || 'Not able to get accounts';
+  });
+};
+window.addEventListener('DOMContentLoaded', initialize)
